ae3633c52808434e412e63b5e1f9e754867f9f31
7eduardocf <eduardocfrota901@gmail.com>
2025-05-08 20:22:55 -0400

    Criando e conectando ao bando de dados

--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,7 @@
-export default{
-    port: 3000
-}
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
+export default {
+    port: 3000,
+    dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.pl4q1ip.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+};
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
 import express from "express"
 import config from "config"
+import router from "./router"
+import connect from "../config/db"
+require("dotenv").config()
+
 
 const app = express()
 
 //JSON middleware
 app.use(express.json())
+app.use("/api/",router)
 
 //APP port
 const port = config.get<number>("port")
 
 app.listen(3000, async()=>{
     console.log("Aplicação funcionando");
-    
+    await connect()
 })
